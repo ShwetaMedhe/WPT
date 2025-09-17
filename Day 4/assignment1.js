9b5377f2468eb95f3ce8d86afd70bec13783a811
Shruti Nitalikar <shrutinitalikar@gmail.com>
2025-09-17 14:07:20 -0700

    Initial commit

--- Day 4/assignment1.js
+++ Day 4/assignment1.js
@@ -1,23 +1,45 @@
-
-function add(...a) {
+/*1) Write a function getFactorialImpl ( choice="FORLOOP" ) ----it could be 
+    FORLOOP or RECUR
+	Function Fact2(n1)
+	Use recursion and print the factorial
+	If the choice is FORLOOP return Fact1
+	If the choice is RECUR then return Fact2
+	Outside --- call let rv = getFactorialImpl("RECUR")
+	rv(3)
+	Rv = getFactorialImpl()
+	rv(5)*/
    
-    let sum = 0;
-    
-    for (let i = 0; i < a.length; i++) {
-        sum += a[i];
-        if(a[i]%2!=0)
-        {
-            console.log("Odd numbers "+a[i])
-        }
+function getFactorial1(n){
+    if(n==0){
+        return 0;
     }
-    
 
-    console.log("Addition of " + (a.length) + " numbers = " + sum);
-    
+    if(n==1){
+        return 1;
+    }
+
+    return n*getFactorial1(n-1);
+}
+
+
+function getFactorial2(n){
+    let fact = 1;
+    for(let i=1;i<=n;i++){
+        fact = fact*i;
+    }
+    console.log("Factorial of "+a+" is "+fact)
+}
+
+let choice = "FORLOOP";
+choice = "Recursive";
+let a = 5;
+switch(choice){
+    case "FORLOOP":
+        getFactorial2(a);
+        break;
+    case "Recursive":
+        let fact = getFactorial1(a);
+        console.log("Factorial of "+a+" is "+fact);
 
 }
 
-add(12, 23, 34);
-add(1, 2);
-add(18, 20, 33);
-add();
